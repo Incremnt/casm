0ff3ea3edf58fe686f41d5f2dedd687e058118ca
Incremnt <stevster27@gmail.com>
2026-05-03 21:54:39 +0400

    add multiple PHDR to hello.asm

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,12 +1,13 @@
 .text
-  call      14
-db "Hello, World!", 10
   mov       eax, 4
   mov       ebx, 1
-  pop       ecx
+  mov       ecx, 134512787
   mov       edx, 14
   int       128
 
   mov       eax, 1
   xor       ebx, ebx
   int       128
+
+.rodata
+db "Hello, World!", 10
