bc35799a74b476d5116c490205c1e6f971c79f37
Incremnt <stevster27@gmail.com>
2026-05-05 22:50:30 +0400

    remove hardcoded addresses from hello.asm

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,13 +1,16 @@
 .text
+  call      14
+
+.rodata
+db "Hello, World!", 10
+
+.text
   mov       eax, 4
   mov       ebx, 1
-  mov       ecx, 134512787
+  pop       ecx
   mov       edx, 14
   int       128
 
   mov       eax, 1
   xor       ebx, ebx
   int       128
-
-.rodata
-db "Hello, World!", 10
