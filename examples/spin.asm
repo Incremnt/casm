582170bbda8bd075d1b72a2b2df6293607aa5b9f
Incremnt <stevster27@gmail.com>
2026-05-05 22:48:46 +0400

    remove hardcoded addresses from spin.asm

--- examples/spin.asm
+++ examples/spin.asm
@@ -1,8 +1,21 @@
 .text
   xor       ebp, ebp
+  call      24
+
+.rodata
+dd 1
+dd 0
+db '|', 27, "[D"
+db '/', 27, "[D"
+db '-', 27, "[D"
+db '\', 27, "[D"
+
+.text
+  pop       esi
   mov       eax, 4
   mov       ebx, 1
-  mov       ecx, 134512831
+  mov       ecx, esi
+  add       ecx, 8
   add       ecx, ebp
   add       ebp, 4
   cmp       ebp, 16
@@ -12,17 +25,9 @@
   int       128
 
   mov       eax, 162
-  mov       ebx, 134512823
+  mov       ebx, esi
   mov       ecx, ebx
   int       128
 
-  mov       edi, 134512758
+  mov       edi, 134512790
   jmp       edi
-
-.rodata
-dd 1
-dd 0
-db '|', 27, "[D"
-db '/', 27, "[D"
-db '-', 27, "[D"
-db '\', 27, "[D"
