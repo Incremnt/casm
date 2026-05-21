4770eba901b9aaf7716c0f7dcfa001a17065abad
Incremnt <stevster27@gmail.com>
2026-05-21 18:44:15 +0400

    add .entry directive in hello.asm

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,11 +1,9 @@
-.text
-  jmp       14
-
 .rodata
 #msg
   db "Hello, World!", 10
 
 .text
+#_start
   mov       eax, 4
   mov       ebx, 1
   mov       ecx, @msg
@@ -15,3 +13,5 @@
   mov       eax, 1
   xor       ebx, ebx
   int       128
+
+.entry @_start
