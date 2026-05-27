e4ad4222c585f2c9ff79f69062c6de47d83fe57c
Incremnt <stevster27@gmail.com>
2026-05-27 16:57:41 +0400

    add usage flag handlers in codegen.asm

--- src/codegen.asm
+++ src/codegen.asm
@@ -21,19 +21,48 @@
 ; License:   GPLv3               ;
 ;================================;
 codegen:
-  mov       rbx, qword [output_fd]            ; write elf header to the output file
-  SYSCALL_3 SYS_WRITE, rbx, ehdr, EHSIZE      ;
+  mov       rbx, qword [output_fd]                               ;
+  cmp       byte [do_gen_elf], 0                                 ; don't generate headers if there was --noelf flag
+  je        dont_gen_elf                                         ;
+  SYSCALL_3 SYS_WRITE, rbx, ehdr, EHSIZE                         ; write elf header to the output file
 
-  mov       rcx, rbp                          ; write program headers
-  sub       rcx, qword [phdrbuf_ptr]          ;
-  mov       rbp, qword [phdrbuf_ptr]
-  SYSCALL_3 SYS_WRITE, rbx, rbp, rcx          ;
+  mov       rcx, rbp                                             ; write program headers
+  sub       rcx, qword [phdrbuf_ptr]                             ;
+  mov       rbp, qword [phdrbuf_ptr]                             ;
+  SYSCALL_3 SYS_WRITE, rbx, rbp, rcx                             ;
 
-  mov       rsi, qword [par_irbuf_ptr]        ; write opcodes
-  mov       rcx, r14                          ;
-  sub       rcx, rsi                          ;
-  SYSCALL_3 SYS_WRITE, rbx, rsi, rcx          ;
+dont_gen_elf:
+  cmp       byte [do_show_bytes], 0                              ; show output file size if there was --bytes flag
+  je        dont_show_bytes                                      ;
+  mov       rsi, qword [par_irbuf_ptr]                           ;
+  mov       rax, r14                                             ;
+  sub       rax, rsi                                             ;
+  mov       rcx, BYTES_BUF_SZ - 1                                ;
+  cmp       byte [do_gen_elf], 0                                 ;
+  je        .convert_file_size                                   ;
+  xor       rdx, rdx                                             ;
+  mov       dx, word [ehdr.phnum]                                ;
+  imul      rdx, rdx, PHENTSIZE                                  ;
+  lea       rax, [rdx + rax + EHSIZE]                            ;
+.convert_file_size:
+  xor       rdx, rdx                                             ;
+  mov       rbp, 10                                              ;
+  div       rbp                                                  ;
+  add       dl, '0'                                              ;
+  mov       byte [bytes_buf + rcx], dl                           ;
+  dec       rcx                                                  ;
+  test      rax, rax                                             ;
+  jnz       .convert_file_size                                   ;
+  SYSCALL_3 SYS_WRITE, STDERR, e_bytes_msg_st, E_BYTES_MSG_ST_SZ ;
+  SYSCALL_3 SYS_WRITE, STDERR, bytes_buf, BYTES_BUF_SZ           ;
+  SYSCALL_3 SYS_WRITE, STDERR, e_bytes_msg_en, E_BYTES_MSG_EN_SZ ;
 
-  SYSCALL_1 SYS_CLOSE, rbx                    ; close file
+dont_show_bytes:
+  mov       rsi, qword [par_irbuf_ptr]                           ; write opcodes
+  mov       rcx, r14                                             ;
+  sub       rcx, rsi                                             ;
+  SYSCALL_3 SYS_WRITE, rbx, rsi, rcx                             ;
+
+  SYSCALL_1 SYS_CLOSE, rbx                                       ; close file
 
 codegen_end = $
