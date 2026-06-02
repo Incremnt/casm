17e1fa6aac3bc2ff4217c6dbf75717a79571d8d3
Incremnt <stevster27@gmail.com>
2026-06-02 22:17:53 +0400

    improve error messages handle in casm.asm

--- src/casm.asm
+++ src/casm.asm
@@ -138,55 +138,71 @@ include "codegen.asm"
 
 ; error handlers
 usage_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_help_msg, E_HELP_MSG_SZ
-  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+  mov       rsi, e_help_msg
+  jmp       err_exit
 
 open_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_open_msg, E_OPEN_MSG_SZ
-  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+  mov       rsi, e_open_msg
+  jmp       err_exit
 
 lseek_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_lseek_msg, E_LSEEK_MSG_SZ
-  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+  mov       rsi, e_lseek_msg
+  jmp       err_exit
 
 brk_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_brk_msg, E_BRK_MSG_SZ
-  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+  mov       rsi, e_brk_msg
+  jmp       err_exit
 
 read_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_read_msg, E_READ_MSG_SZ
-  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+  mov       rsi, e_read_msg
+  jmp       err_exit
 
 unk_tkn_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_unktkn_msg, E_UNKTKN_MSG_SZ
-  jmp       err_exit
+  mov       rsi, e_unktkn_msg
+  jmp       err_exit_line
 
 long_num_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_longnum_msg, E_LONGNUM_MSG_SZ
-  jmp       err_exit
+  mov       rsi, e_longnum_msg
+  jmp       err_exit_line
 
 invalid_char_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_invalid_char_msg, E_INVALID_CHAR_MSG_SZ
-  jmp       err_exit
+  mov       rsi, e_invalid_char_msg
+  jmp       err_exit_line
 
 invalid_expression_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_invalid_expr_msg, E_INVALID_EXPR_MSG_SZ
-  jmp       err_exit
+  mov       rsi, e_invalid_expr_msg
+  jmp       err_exit_line
 
 op_sz_not_match_err:
-  SYSCALL_3 SYS_WRITE, STDERR, e_op_sz_match_msg, E_OP_SZ_MATCH_MSG_SZ
-  jmp       err_exit
+  mov       rsi, e_op_sz_match_msg
+  jmp       err_exit_line
 
 undef_lbl_err:
-  mov       rax, '?'
-  mov       qword [line_buf], rax
-  SYSCALL_3 SYS_WRITE, STDERR, e_undef_lbl_msg, E_UNDEF_LBL_MSG_SZ
-  SYSCALL_3 SYS_WRITE, STDERR, e_line_msg_st, E_LINE_MSG_ST_SZ
-  SYSCALL_3 SYS_WRITE, STDERR, line_buf, LINE_BUF_SZ
-  SYSCALL_3 SYS_WRITE, STDERR, e_line_msg_end, E_LINE_MSG_END_SZ
-  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+  mov       rsi, e_undef_lbl_msg
+  jmp       err_exit_line
 
 err_exit:
+  xor       rcx, rcx
+  push      rsi
+.calc_size:
+  inc       rcx
+  inc       rsi
+  cmp       byte [rsi], NUL
+  jne       .calc_size
+  pop       rsi
+  SYSCALL_3 SYS_WRITE, STDERR, rsi, rcx
+  SYSCALL_1 SYS_EXIT, EXIT_FAILURE
+
+err_exit_line:
+  xor       rcx, rcx
+  push      rsi
+.calc_size:
+  inc       rcx
+  inc       rsi
+  cmp       byte [rsi], NUL
+  jne       .calc_size
+  pop       rsi
+  SYSCALL_3 SYS_WRITE, STDERR, rsi, rcx
   mov       rax, qword [current_line]
   mov       rcx, LINE_BUF_SZ - 1
 .convert_line:
@@ -209,57 +225,34 @@ err_exit:
 segment readable writable
 
 ; error messages
-e_open_msg         db ESC, '[31m', "[Error]: Can't open file", ESC, '[0m', LF
-E_OPEN_MSG_SZ         = $ - e_open_msg
-
-e_lseek_msg        db ESC, '[31m', "[Error]: SYS_LSEEK failed", ESC, '[0m', LF
-E_LSEEK_MSG_SZ        = $ - e_lseek_msg
-
-e_brk_msg          db ESC, '[31m', "[Error]: Can't allocate memory", ESC, '[0m', LF
-E_BRK_MSG_SZ          = $ - e_brk_msg
-
-e_read_msg         db ESC, '[31m', "[Error]: SYS_READ failed", ESC, '[0m', LF
-E_READ_MSG_SZ         = $ - e_read_msg
-
-e_unktkn_msg       db ESC, '[31m', "[Error]: Unknown token", ESC, '[0m', LF
-E_UNKTKN_MSG_SZ       = $ - e_unktkn_msg
-
-e_longnum_msg      db ESC, '[31m', "[Error]: Number is too long", ESC, '[0m', LF
-E_LONGNUM_MSG_SZ      = $ - e_longnum_msg
-
-e_invalid_char_msg db ESC, '[31m', "[Error]: Unexpected character in string", ESC, '[0m', LF
-E_INVALID_CHAR_MSG_SZ = $ - e_invalid_char_msg
-
-e_invalid_expr_msg db ESC, '[31m', "[Error]: Invalid expression", ESC, '[0m', LF
-E_INVALID_EXPR_MSG_SZ = $ - e_invalid_expr_msg
-
-e_op_sz_match_msg  db ESC, '[31m', "[Error]: Operand size is not match", ESC, '[0m', LF
-E_OP_SZ_MATCH_MSG_SZ  = $ - e_op_sz_match_msg
-
-e_undef_lbl_msg    db ESC, '[31m', "[Error]: Undefined label", ESC, '[0m', LF
-E_UNDEF_LBL_MSG_SZ    = $ - e_undef_lbl_msg
+e_open_msg         db ESC, '[31m', "[Error]: Can't open file",                ESC, '[0m', LF, NUL
+e_lseek_msg        db ESC, '[31m', "[Error]: SYS_LSEEK failed",               ESC, '[0m', LF, NUL
+e_brk_msg          db ESC, '[31m', "[Error]: Can't allocate memory",          ESC, '[0m', LF, NUL
+e_read_msg         db ESC, '[31m', "[Error]: SYS_READ failed",                ESC, '[0m', LF, NUL
+e_unktkn_msg       db ESC, '[31m', "[Error]: Unknown token",                  ESC, '[0m', LF, NUL
+e_longnum_msg      db ESC, '[31m', "[Error]: Number is too long",             ESC, '[0m', LF, NUL
+e_invalid_char_msg db ESC, '[31m', "[Error]: Unexpected character in string", ESC, '[0m', LF, NUL
+e_invalid_expr_msg db ESC, '[31m', "[Error]: Invalid expression",             ESC, '[0m', LF, NUL
+e_op_sz_match_msg  db ESC, '[31m', "[Error]: Operand size is not match",      ESC, '[0m', LF, NUL
+e_undef_lbl_msg    db ESC, '[31m', "[Error]: Undefined label",                ESC, '[0m', LF, NUL
 
 e_line_msg_st      db ESC, '[31m', "[Line]:  "
 E_LINE_MSG_ST_SZ      = $ - e_line_msg_st
-
 e_line_msg_end     db ESC, '[0m', LF
 E_LINE_MSG_END_SZ     = $ - e_line_msg_end
 
 e_bytes_msg_st     db "[Size]:  "
 E_BYTES_MSG_ST_SZ     = $ - e_bytes_msg_st
-
 e_bytes_msg_en     db " bytes", LF
 E_BYTES_MSG_EN_SZ     = $ - e_bytes_msg_en
 
 e_style_msg_st     db "[Style]: "
 E_STYLE_MSG_ST_SZ     = $ - e_style_msg_st
-
 e_style_msg_en     db "%", LF
 E_STYLE_MSG_EN_SZ     = $ - e_style_msg_en
 
 e_rank_msg_st      db "[Rank]:  "
 E_RANK_MSG_ST_SZ      = $ - e_rank_msg_st
-
 e_rank_msg_en      db LF
 E_RANK_MSG_EN_SZ      = $ - e_rank_msg_en
 
