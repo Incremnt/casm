de9ece7e0e292168a3fb9e8fa04ae5b72593aac8
Incremnt <stevster27@gmail.com>
2026-06-22 17:44:24 +0400

    fix ranks' colors in casm.asm

--- src/casm.asm
+++ src/casm.asm
@@ -334,12 +334,12 @@ do_show_bytes  db 0
 do_show_rank   db 0
 
 ; ranks
-st_p           db ESC, "[48;5;003m", " P ", ESC, "[0m"
-st_s           db ESC, "[38;5;001m", "S  ", ESC, "[0m"
-st_a           db ESC, "[38;5;172m", "A  ", ESC, "[0m"
-st_b           db ESC, "[38;5;003m", "B  ", ESC, "[0m"
-st_c           db ESC, "[38;5;002m", "C  ", ESC, "[0m"
-st_d           db ESC, "[38;5;004m", "D  ", ESC, "[0m"
+st_p           db ESC, "[38;5;254m", ESC, "[48;5;214m", " P ", ESC, "[0m"
+st_s           db ESC, "[38;5;254m", ESC, "[38;5;196m", "S  ", ESC, "[0m"
+st_a           db ESC, "[38;5;254m", ESC, "[38;5;208m", "A  ", ESC, "[0m"
+st_b           db ESC, "[38;5;254m", ESC, "[38;5;214m", "B  ", ESC, "[0m"
+st_c           db ESC, "[38;5;254m", ESC, "[38;5;112m", "C  ", ESC, "[0m"
+st_d           db ESC, "[38;5;254m", ESC, "[38;5;069m", "D  ", ESC, "[0m"
 RANK_SZ           = $ - st_d
 
 ; pointers
